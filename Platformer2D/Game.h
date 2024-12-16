3c2cb539483cacec408502777b587c434d2c2b85
levi <leviamullins@gmail.com>
2024-12-16 17:20:09 -0500

    fixed issues with balls not bouncing on floor impact

--- Platformer2D/Game.h
+++ Platformer2D/Game.h
@@ -28,6 +28,9 @@ private:
 	//constants
 	const float gravity = 500.0f;
 
+	//nonConstants
+	std::vector<float> bounceVal;
+
 	void initVars();
 	void initWindow();
 	void initCircle();
@@ -56,6 +59,23 @@ public:
 				this->velocityY[i] += gravity * deltaTime;
 			}
 
+			this->circles[i].move(0.0f, this->velocityY[i] * deltaTime);
+			//calculate bottom of circle
+			float circleBottom = this->circles[i].getPosition().y + this->circles[i].getRadius() * 2;
+
+			if (circleBottom >= 600.0f)
+			{
+				this->velocityY[i] = -this->velocityY[i];
+
+				if (this->bounceVal[i] <= 0.0f)
+				{
+					this->circles[i].setPosition(this->circles[i].getPosition().x, 600.0f - this->circles[i].getRadius() * 2);
+					this->bounceVal[i] = 0.0f;
+					this->velocityY[i] = 0.0f;
+					this->onGround[i] = true;
+				}
+			}
+
 			for (int j = 0; j < this->circles.size(); j++)
 			{
 				if (i != j)
@@ -73,30 +93,13 @@ public:
 					{
 						float movecirc = (radA + radB) - dist;
 						sf::Vector2f moveDir(distX / dist, distY / dist);
-						this->circles[i].move(moveDir.x * movecirc * 0.2f, moveDir.y * movecirc * 0.2f);
-						this->circles[j].move(-moveDir.x * movecirc * 0.2f, -moveDir.y * movecirc * 0.2f);
+						this->circles[i].move(moveDir.x * movecirc * 0.5f, moveDir.y * movecirc * 0.5f);
+						this->circles[j].move(-moveDir.x * movecirc * 0.5f, -moveDir.y * movecirc * 0.5f);
 
-						//BOUNCE PHYSICS
-						this->velocityX[i] = -this->velocityX[i];
-						this->velocityX[j] = -this->velocityX[j];
-						this->velocityY[i] = -this->velocityY[i];
-						this->velocityY[j] = -this->velocityY[j];
-
+						//Bounce physics
+						std::swap(this->velocityY[i], this->velocityY[j]);
+						std::swap(this->velocityX[i], this->velocityX[j]);
 					}
-				}
-			}
-
-
-			for (int i = 0; i < this->circles.size(); i++)
-			{
-				float circleBottom = this->circles[i].getPosition().y + this->circles[i].getRadius() * 2;
-				this->circles[i].move(0.0f, this->velocityY[i] * deltaTime);
-
-				if (circleBottom >= 600.0f)
-				{
-					this->circles[i].setPosition(this->circles[i].getPosition().x, 600.0f - this->circles[i].getRadius() * 2);
-					this->velocityY[i] = 0.0f;
-					this->onGround[i] = true;
 				}
 			}
 		}
